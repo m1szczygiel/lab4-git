080a0b47e73700e0b5e71a67638a1da3035f16e9
Michał <michalszczygiel@vp.pl>
2025-05-07 13:37:04 +0200

    Dodano tworzenie logów

--- skrypt.sh
+++ skrypt.sh
@@ -1,20 +1,24 @@
-#!/bin/bash
-
-while [[ $# -gt 0 ]]; do
-    case $1 in
-        --date|-d)
-            date
-            shift
-            ;;
-        --help|-h)
-            echo "Dostępne opcje:"
-            echo "--date | -d - wyświetla datę"
-            echo "--logs | -l - tworzy pliki log"
-            shift
-            ;;
-        *)
-            echo "Nieznana opcja: $1"
-            exit 1
-            ;;
-    esac
-done
+case $1 in
+    --date|-d)
+        date
+        shift
+        ;;
+    --logs|-l)
+        count=${2:-100}
+        for i in $(seq 1 $count); do
+            echo "log$i.txt, skrypt.sh, $(date)" > log$i.txt
+        done
+        shift
+        shift
+        ;;
+    --help|-h)
+        echo "Dostępne opcje:"
+        echo "--date | -d - wyświetla datę"
+        echo "--logs | -l - tworzy pliki log"
+        shift
+        ;;
+    *)
+        echo "Nieznana opcja: $1"
+        exit 1
+        ;;
+esac
